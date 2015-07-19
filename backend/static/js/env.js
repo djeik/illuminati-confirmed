9fb50b552b556bf2873ae721b0f35c807a66fea1
Kevin Li <kevin@mail.ahvoda.com>
2015-07-19 07:48:23 -0400

    Implement pop-up share after loading photo2

--- backend/static/js/env.js
+++ backend/static/js/env.js
@@ -23,15 +23,18 @@ $("input[type='file']").on("change",function(event){
         processData: false,
         contentType: false,
         success: function(data, textStatus, jqXHR) {
-            console.log('Ajax succeeded');
             $("#title").text("Illuminati Confirmed!");
             $("#photo2").load(function(){
-                console.log('photo2 loaded');
                 $("#photo").hide();
                 $("#photo2").show();
             });
             $("#photo2").attr("src", data.url);
+            $("meta[property='og:image']").attr("content", "www.illuminaticonfirmed.xyz" + data.url);
             $("#spinner").fadeOut(5000);
+            FB.ui({
+                  method: 'share',
+                    href: 'http://www.illuminaticonfirmed.xyz',
+            }, function(response){});
         },
         error: function(error) {
             console.log('error occurred', error);
