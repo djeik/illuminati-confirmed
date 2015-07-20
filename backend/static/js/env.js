7a9be85cf86fd12eea3dadccf6c3d0554d387a33
Jacob Errington <git@mail.jerrington.me>
2015-07-19 20:56:56 -0400

    generalize web page layout and add routes for new pages in backend

--- backend/static/js/env.js
+++ backend/static/js/env.js
@@ -9,16 +9,28 @@ $(document).ready(function() {
     var input = $("input[type='file']");
     input.attr("disabled", false);
 
+    // When the user selects an image.
     input.on("change", function(event) {
+        // Extract the selected file
         var file = event.target.files[0];
+
+        // Create a file reader to show the image
         var reader = new FileReader();
+
+        // When the reader has loaded the image as a data URL, we set the src
+        // attribute of the main image to that data url, which makes the
+        // browser load that image.
         reader.onload = function(event){
-            $("#photo").attr("src", event.target.result);
-            $("#photo").show();
+            console.log('reader loaded');
+            var photo = $("#photo");
             $("#upload-icon").hide();
-            $("#spinner").fadeIn(5000);
-            $("#title").text("Confirming Illuminati...");
-            $("#file-upload-input").attr("disabled", "disabled");
+            photo.load(function() {
+                $("#spinner").fadeIn(5000);
+                $("#title").text("Confirming Illuminati...");
+                $("#file-upload-input").attr("disabled", "disabled");
+            });
+            photo.attr("src", event.target.result);
+            photo.show();
         }
         reader.readAsDataURL(file);
 
@@ -38,10 +50,14 @@ $(document).ready(function() {
                     "app_id=982515815140018" +
                     "&display=popup" +
                     "&href=" + "http%3A%2F%2Fwww.illuminaticonfirmed.xyz:6501" + encodeURIComponent(data.id) +
-                    "&redirect_uri=" + encodeURIComponent(window.location.href); 
+                    "&redirect_uri=" + encodeURIComponent(window.location.href);
 
-                console.log('Ajax succeeded');
+                console.log('Ajax succeeded', JSON.stringify(data));
+                // Change header text
                 $("#title").text("Illuminati Confirmed!");
+
+                // When the new image is loaded, replace the initial image with
+                // the new image
                 $("#photo2").load(function(){
                     console.log('photo2 loaded');
                     $("#photo").hide();
@@ -49,12 +65,19 @@ $(document).ready(function() {
                     $("pluginShareButtonLink").attr("href", "/sharer/sharer.php?app_id=982515815140018&amp;sdk=joey&amp;u=http%3A%2F%2Fwww.illuminaticonfirmed.xyz%3A6501%2F" +
                         data.id + "&amp;display=popup&amp;ref=plugin&amp;src=share_button"
                 });
+
+                // Load the new image
                 $("#photo2").attr("src", data.url);
+
+                // Fade out the spinner for five seconds
                 $("#spinner").fadeOut(5000);
+
+                // Play the xfiles music
                 playIlluminati();
             },
             error: function(error) {
-                console.log('error occurred', error);
+                $("#title").text("Can't confirm!");
+                $(".container").append($("<p>They're watching...</p>"));
             }
         });
     });
--- backend/views.py
+++ backend/views.py
@@ -4,6 +4,13 @@ from . import app, basedir
 
 import os
 
+def illuminati_path(id):
+    return os.path.join(
+            basedir,
+            app.config['ILLUMINATI_OUTPUT_DIR'],
+            id + '.' + app.config['ILLUMINATI_FORMAT'],
+    )
+
 @app.route('/')
 def index():
     return render_template(
@@ -12,11 +19,19 @@ def index():
 
 @app.route('/illuminati/<id>')
 def illuminati(id):
+    path = illuminati_path(id)
+
+    if not os.path.exists(path):
+        abort(400)
+
     return send_file(
-            os.path.join(
-                basedir,
-                app.config['ILLUMINATI_OUTPUT_DIR'],
-                id + '.' + app.config['ILLUMINATI_FORMAT'],
-            ),
+            path,
             conditional=True,
+    )
+
+@app.route('/<id>')
+def illuminati_page(id):
+    return render_template(
+            'illuminati.html',
+            id=id,
     )
